5079dd244d8ff4fa606d961d395ba72ce78ec3ce
Grant Richmond <mail@grant.codes>
2018-01-07 20:50:01 +0000

    remove leftover leaflet stuff

--- src/components/card.js
+++ src/components/card.js
@@ -24,17 +24,6 @@ import moment from 'moment';
 import authorToAvatarData from '../modules/author-to-avatar-data';
 import * as indieActions from '../modules/indie-actions';
 
-// Hack to fix leaflet marker
-import L from 'leaflet';
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-// End hacky times
-
 const styles = theme => ({
   card: {
     marginTop: 12,
