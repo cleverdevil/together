d6de42d976bda26692a70ed11bd8eba42b0d3640
Grant Richmond <mail@grant.codes>
2018-02-16 19:42:08 -0600

    make link colors less horrible

--- src/components/card.js
+++ src/components/card.js
@@ -41,6 +41,12 @@ const styles = theme => ({
     marginTop: 12,
     marginBottom: 12,
     overflow: 'hidden',
+    '& a': {
+      color:
+        theme.palette.type === 'dark'
+          ? theme.palette.secondary.light
+          : theme.palette.secondary.main,
+    },
   },
   fullImage: {
     display: 'block',
